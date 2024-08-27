c109d624fa9e0c0a28e85420308888bd27038685
Carlos Pinho <72576083+carlospinhod@users.noreply.github.com>
2024-08-27 23:24:11 +0100

    EP 18
    Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let message = ref('Hello World!')
+
+let doSomething = () => {
+  alert('doing it now')
+}
+setTimeout(() => {
+  message.value = 'I have been changed'
+}, 2000)
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p> {{ message }} </p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
+    <p>
+      <button @click="doSomething">Click me!</button>
+    </p>
   </main>
 </template>
